226492c7b6c3236d3de2e2296228c5479909863d
jwywoo <jwywoo26@gmail.com>
2023-02-10 21:45:26 -0500

    Cash Card: Testing first done

--- src/test/java/com/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/example/cashcard/CashCardApplicationTests.java
@@ -2,12 +2,28 @@ package com.example.cashcard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class CashCardApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void myFirstTest() {
+		assertThat(1).isEqualTo(1);
 	}
 
 }
+
+//package example.cashcard;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//public class CashCardJsonTest {
+//
+//@Test
+//    public void myFirstTest() {
+//        assertThat(1).isEqualTo(42);
+//    }
+//}
