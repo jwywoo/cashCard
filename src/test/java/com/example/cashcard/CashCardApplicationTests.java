16944c2f9865b2f871198d221e79a89523db4b10
jwywoo <jwywoo26@gmail.com>
2023-02-20 20:18:13 -0500

    cashCards: Implementing GET

--- src/test/java/com/example/cashcard/CashCardApplicationTests.java
+++ src/test/java/com/example/cashcard/CashCardApplicationTests.java
@@ -1,29 +1,33 @@
 package com.example.cashcard;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
+//@SpringBootTest
+//class CashCardApplicationTests {
+//
+//	@Test
+//	public void myFirstTest() {
+//		assertThat(1).isEqualTo(1);
+//	}
+//
+//}
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CashCardApplicationTests {
+	@Autowired
+	TestRestTemplate restTemplate;
 
 	@Test
-	public void myFirstTest() {
-		assertThat(1).isEqualTo(1);
+	void shouldReturnCashCardWhenDataIsSaved() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/cashcards/99", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
-
 }
-
-//package example.cashcard;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import static org.assertj.core.api.Assertions.assertThat;
-//
-//public class CashCardJsonTest {
-//
-//@Test
-//    public void myFirstTest() {
-//        assertThat(1).isEqualTo(42);
-//    }
-//}
