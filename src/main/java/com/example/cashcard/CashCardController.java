c5a1ec121f6232e6fedf65d8968a9f86ced0cc65
jwywoo <jwywoo26@gmail.com>
2023-02-20 20:28:24 -0500

    cashCards: Implementing GET - main/java/cashCardController.java comment done

--- src/main/java/com/example/cashcard/CashCardController.java
+++ src/main/java/com/example/cashcard/CashCardController.java
@@ -5,10 +5,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-
+// Annotation this class is Controller to handle request
 @RestController
+// Annotation indicating which address requests must have to access this controller
 @RequestMapping("/cashcards")
 public class CashCardController {
+    // Annotation that shows this is handling GET
     @GetMapping("/{requestId}")
     public ResponseEntity<String> findById() {
         return ResponseEntity.ok("{}");
